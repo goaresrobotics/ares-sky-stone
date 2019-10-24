949f54731cad43450dbdc03fbe3a9fe2232d2d80
Jason <jjmack921@gmail.com>
2019-10-24 09:33:23 -0700

    Worked more on Vuforia to try and figure out if the code returned location in the right way. Unfinished

--- src/com/aresrobotics/test/vuforiaTest.java
+++ src/com/aresrobotics/test/vuforiaTest.java
@@ -1,6 +1,7 @@
 package com.aresrobotics.test;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.RobotLog;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
@@ -17,6 +18,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import java.util.ArrayList;
 import java.util.List;
 
+@TeleOp(name = "Vuforia Test")
 public class vuforiaTest extends LinearOpMode {
 
     OpenGLMatrix lastLocation;
@@ -35,53 +37,63 @@ public class vuforiaTest extends LinearOpMode {
 
         vuforia = ClassFactory.getInstance().createVuforia(parameters);
 
-        //after assetName: write the name of the .XML dataset to load in quotes(Skystone is just a temporary name)
+        //after assetName: write the name of the .XML dataset to load in quotes
         VuforiaTrackables Skystone = this.vuforia.loadTrackablesFromAsset("Skystone");
 
         //C3P0 is the name of one of the images, 0 shows that it is the first image
-        VuforiaTrackable redPerimeterTarget1 = Skystone.get(0);
-        redPerimeterTarget1.setName("redPerimeterTarget1");
+        VuforiaTrackable TargetElement = Skystone.get(0);
+        TargetElement.setName("TargetElement");
 
 
         List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>();
         allTrackables.addAll(Skystone);
 
         float mmPerInch = 25.4f;
-        float mmFTCFieldWidth = (12 * 12 - 2) * mmPerInch;
+        float mmFTCFieldWidth = (12 * 12-2) * mmPerInch;
 
-        OpenGLMatrix redBlockSideBridgeTarget1 = OpenGLMatrix.translation(mmFTCFieldWidth / 2, 13 * mmPerInch, 0).multiplied(Orientation.getRotationMatrix
-                (AxesReference.EXTRINSIC, AxesOrder.XZX, AngleUnit.DEGREES, 90, 90, 0));
-        redPerimeterTarget1.setLocation(redBlockSideBridgeTarget1);
+        OpenGLMatrix redBlockSideBridgeTarget1 = OpenGLMatrix.translation(0, 0, 0).multiplied(Orientation.getRotationMatrix
+                (AxesReference.EXTRINSIC, AxesOrder.XZX, AngleUnit.DEGREES, 0, 0, 0));
+        TargetElement.setLocation(redBlockSideBridgeTarget1);
 
-        OpenGLMatrix phoneLocationOnRobot = OpenGLMatrix.translation(0, 0, 0).multiplied(Orientation.getRotationMatrix
+        OpenGLMatrix phoneLocation = OpenGLMatrix.translation(0, 0, 0).multiplied(Orientation.getRotationMatrix
                 (AxesReference.EXTRINSIC, AxesOrder.YZY, AngleUnit.DEGREES, -90, 0, 0));
 
-        ((VuforiaTrackableDefaultListener) redPerimeterTarget1.getListener()).setPhoneInformation(phoneLocationOnRobot, parameters.cameraDirection);
+        ((VuforiaTrackableDefaultListener) TargetElement.getListener()).setPhoneInformation(phoneLocation, parameters.cameraDirection);
 
         telemetry.addData(">", "Wait for start");
         telemetry.update();
+
         waitForStart();
 
         telemetry.clear();
+        telemetry.update();
 
         Skystone.activate();
 
         while (opModeIsActive()) {
             for (VuforiaTrackable trackable : allTrackables) {
-
-                OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)trackable.getListener()).getUpdatedRobotLocation();
+                OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener) trackable.getListener()).getUpdatedRobotLocation();
                 if (robotLocationTransform != null) {
                     lastLocation = robotLocationTransform;
                 }
             }
 
-            if(lastLocation == null) {
+            telemetry.clear();
+
+            if (lastLocation == null) {
                 telemetry.addData("Location:", "Unknown");
             } else {
-                telemetry.addData("Location:", lastLocation);
+                telemetry.addData("Location:", format(lastLocation));
             }
+
+            telemetry.update();
 
         }
 
     }
+
+    String format(OpenGLMatrix transformationMatrix) {
+        return transformationMatrix.formatAsTransform();
+    }
+
 }
