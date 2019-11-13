ef2e103df66ed5221bdbe240e190dff19ad6b0d0
Jason <jjmack921@gmail.com>
2019-11-12 18:00:08 -0800

    Worked more on ToF Coordinate Test

--- src/com/aresrobotics/test/timeOfFlightCoordinateTest.java
+++ src/com/aresrobotics/test/timeOfFlightCoordinateTest.java
@@ -2,6 +2,9 @@ package com.aresrobotics.test;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class timeOfFlightCoordinateTest extends OpMode {
 
@@ -12,8 +15,18 @@ public class timeOfFlightCoordinateTest extends OpMode {
 
     private double x;
     private double y;
-    private double orientation;
+    private double previousX;
+    private double previousY;
 
+    private double orientation;
+    private double leftDistance;
+    private double rightDistance;
+    private double backDistance;
+    private double frontDistance;
+
+    private boolean hasTimeStarted = false;
+
+    public ElapsedTime passedTime;
 
     @Override
     public void init() {
@@ -28,7 +41,37 @@ public class timeOfFlightCoordinateTest extends OpMode {
     @Override
     public void loop() {
 
-    
+        if(left.getDistance(DistanceUnit.INCH) < 78.7401574803){
+            x = -141.23/2 + left.getDistance(DistanceUnit.INCH);
+        } else {
+            if(right.getDistance(DistanceUnit.INCH) < 78.7401574803){
+                x = 141.23/2 - right.getDistance(DistanceUnit.INCH);
+            } else {
+                //make this useless if it happens(this only happens if both sensors aren't working/out of range
+                x = 100;
+            }
+        }
+
+        if(front.getDistance(DistanceUnit.INCH) < 78.7401574803){
+            y = 141.23/2 - front.getDistance(DistanceUnit.INCH);
+        } else {
+            if(back.getDistance(DistanceUnit.INCH) < 78.7401574803){
+                y = -141.23/2 + back.getDistance(DistanceUnit.INCH);
+            } else {
+                //make this useless if it happens(this only happens if both sensors aren't working/out of range
+                y = 100;
+            }
+        }
+
+        if(!hasTimeStarted) {
+            passedTime.startTime();
+            hasTimeStarted = true;
+        }
+
+        if(passedTime.milliseconds() > 100){
+            passedTime.reset();
+            //if()
+        }
 
     }
 }
