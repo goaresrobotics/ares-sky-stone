d9d918afa0604ca5d2e7dfaa50023dfeecb4a57b
Jason <jjmack921@gmail.com>
2019-11-14 20:13:15 -0800

    Continued work on ToFCoordinateTest code

--- src/com/aresrobotics/test/timeOfFlightCoordinateTest.java
+++ src/com/aresrobotics/test/timeOfFlightCoordinateTest.java
@@ -18,6 +18,12 @@ public class timeOfFlightCoordinateTest extends OpMode {
     private double previousX;
     private double previousY;
 
+    private double displacedX;
+    private double displacedY;
+
+    private boolean isThereDisplacementX;
+    private boolean isThereDisplacementY;
+
     private double orientation;
     private double leftDistance;
     private double rightDistance;
@@ -31,47 +37,83 @@ public class timeOfFlightCoordinateTest extends OpMode {
     @Override
     public void init() {
 
-         left = hardwareMap.get(DistanceSensor.class, "left");
-         right = hardwareMap.get(DistanceSensor.class, "right");
-         back = hardwareMap.get(DistanceSensor.class, "back");
-         front = hardwareMap.get(DistanceSensor.class, "front");
+        left = hardwareMap.get(DistanceSensor.class, "left");
+        right = hardwareMap.get(DistanceSensor.class, "right");
+        back = hardwareMap.get(DistanceSensor.class, "back");
+        front = hardwareMap.get(DistanceSensor.class, "front");
+        hasTimeStarted = false;
 
     }
 
     @Override
     public void loop() {
 
-        if(left.getDistance(DistanceUnit.INCH) < 78.7401574803){
-            x = -141.23/2 + left.getDistance(DistanceUnit.INCH);
+        if (left.getDistance(DistanceUnit.INCH) < 78.7401574803) {
+            x = -141.23 / 2 + left.getDistance(DistanceUnit.INCH);
         } else {
-            if(right.getDistance(DistanceUnit.INCH) < 78.7401574803){
-                x = 141.23/2 - right.getDistance(DistanceUnit.INCH);
+            if (right.getDistance(DistanceUnit.INCH) < 78.7401574803) {
+                x = 141.23 / 2 - right.getDistance(DistanceUnit.INCH);
             } else {
                 //make this useless if it happens(this only happens if both sensors aren't working/out of range
                 x = 100;
             }
         }
 
-        if(front.getDistance(DistanceUnit.INCH) < 78.7401574803){
-            y = 141.23/2 - front.getDistance(DistanceUnit.INCH);
+        if (front.getDistance(DistanceUnit.INCH) < 78.7401574803) {
+            y = 141.23 / 2 - front.getDistance(DistanceUnit.INCH);
         } else {
-            if(back.getDistance(DistanceUnit.INCH) < 78.7401574803){
-                y = -141.23/2 + back.getDistance(DistanceUnit.INCH);
+            if (back.getDistance(DistanceUnit.INCH) < 78.7401574803) {
+                y = -141.23 / 2 + back.getDistance(DistanceUnit.INCH);
             } else {
                 //make this useless if it happens(this only happens if both sensors aren't working/out of range
                 y = 100;
             }
         }
 
-        if(!hasTimeStarted) {
+        if (!hasTimeStarted) {
+
             passedTime.startTime();
             hasTimeStarted = true;
+            previousX = x;
+
         }
 
-        if(passedTime.milliseconds() > 100){
+        if (xInterruption(passedTime.milliseconds()) == true) {
+
+            previousX = x;
+            isThereDisplacementX = false;
+
+        }
+
+
+        if (xInterruption(passedTime.milliseconds()) == false ) {
+
+            displacedX = previousX - x;
+            isThereDisplacementX = true;
+
+        }
+
+        if (passedTime.milliseconds() >= 100) {
+
             passedTime.reset();
-            //if()
+
         }
 
     }
+
+    public boolean xInterruption(double timePassed) {
+
+        if (timePassed >= 100 && x - previousX > 3) {
+
+            return false;
+
+        } else {
+
+            return true;
+
+        }
+
+
+    }
+
 }
