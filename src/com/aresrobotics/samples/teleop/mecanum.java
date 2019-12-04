bcfeaa820e653694f01efb328482166e9d78f348
unknown <nipayrhk@gmail.com>
2019-12-04 14:30:58 -0500

    Made mecanum fully non dependant on direction

--- src/com/aresrobotics/samples/teleop/mecanum.java
+++ src/com/aresrobotics/samples/teleop/mecanum.java
@@ -4,6 +4,9 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import java.lang.Math;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 @TeleOp(name = "mecanum")
 public class    mecanum extends OpMode {
@@ -13,6 +16,7 @@ public class    mecanum extends OpMode {
     private DcMotor motorLeftBack;
     private DcMotor motorRight;
     private DcMotor motorRightBack;
+    private BNO055IMU gyrosensor;
 
     @Override
     public void init()
@@ -22,6 +26,12 @@ public class    mecanum extends OpMode {
         motorLeftBack = hardwareMap.dcMotor.get("motorLeftBack");
         motorRight = hardwareMap.dcMotor.get("motorRight");
         motorRightBack = hardwareMap.dcMotor.get("motorRightBack");
+        gyrosensor = hardwareMap.get(BNO055IMU.class, "imu");
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = (BNO055IMU.AngleUnit.DEGREES);
+        gyrosensor.initialize(parameters);
+
 
     }
 
@@ -29,11 +39,22 @@ public class    mecanum extends OpMode {
     public void loop()
     {
 
+        Orientation orientation  = gyrosensor.getAngularOrientation();
+        telemetry.addData("1", orientation.firstAngle);
 
-        double angle = 0;
+        double a = orientation.firstAngle;
+        double x1 = gamepad1.left_stick_x;
+        double y1 = gamepad1.left_stick_y;
 
-        double newX = Math.sqrt(Math.pow(gamepad1.left_stick_x, 2)+ Math.pow(gamepad1.left_stick_y, 2))*Math.cos(angle-Math.acos((gamepad1.left_stick_x)/Math.sqrt((Math.pow(gamepad1.left_stick_x, 2)+ Math.pow(gamepad1.left_stick_y, 2)))));
-        double newY = Math.sqrt(Math.pow(gamepad1.left_stick_x, 2)+ Math.pow(gamepad1.left_stick_y, 2))*Math.sin(angle-Math.acos((gamepad1.left_stick_x)/Math.sqrt((Math.pow(gamepad1.left_stick_x, 2)+ Math.pow(gamepad1.left_stick_y, 2)))));
+        double newX = Math.sqrt(Math.pow(x1, 2)+ Math.pow(y1, 2))*Math.cos(a-Math.acos((x1)/Math.sqrt((Math.pow(x1, 2)+ Math.pow(y1, 2)))));
+        double newY = Math.sqrt(Math.pow(x1, 2)+ Math.pow(y1, 2))*Math.sin(a-Math.acos((x1)/Math.sqrt((Math.pow(x1, 2)+ Math.pow(y1, 2)))));
+
+        telemetry.addData("newX",newX);
+        telemetry.addData("newY",newY);
+        telemetry.addData("angle",a);
+        telemetry.addData("gamepad y",y1);
+        telemetry.addData("gamepad x",x1);
+        telemetry.update();
 
         double h = Math.hypot(gamepad1.left_stick_x, -gamepad1.left_stick_y);
         double robotAngle = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
