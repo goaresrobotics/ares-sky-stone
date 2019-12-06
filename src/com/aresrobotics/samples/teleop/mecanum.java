275bda1f15fe66ad925145b3779874e9b483ba31
unknown <nipayrhk@gmail.com>
2019-12-06 14:11:51 -0500

    fixed mecanum

--- src/com/aresrobotics/samples/teleop/mecanum.java
+++ src/com/aresrobotics/samples/teleop/mecanum.java
@@ -16,27 +16,6 @@ public class    mecanum extends OpMode {
     private DcMotor motorRight;
     private DcMotor motorRightBack;
 
-    private double LDistanceInches;
-    private double LBDistanceInches;
-    private double RDistanceInches;
-    private double RBDistanceInches;
-
-    /*private double previousPosL = 0;
-    private double previousPosLB = 0;
-    private double previousPosR = 0;
-    private double previousPosRB = 0;
-
-    private double rightDifference = 0;
-    private double rightBackDifference = 0;
-    private double leftDifference = 0;
-    private double leftBackDifference = 0;*/
-
-    static final double COUNTS_PER_MOTOR_REV = 560;
-    static final double DRIVE_GEAR_REDUCTION = -1;
-    static final double WHEEL_DIAMETER_INCHES = 4;
-    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
-
-    //private boolean isTimeStarted = false;
     @Override
     public void init()
     {
@@ -51,21 +30,6 @@ public class    mecanum extends OpMode {
     @Override
     public void loop()
     {
-        /*if(!isTimeStarted) {
-
-            runtime.reset();
-            isTimeStarted = true;
-        }*/
-
-        LDistanceInches = motorLeft.getCurrentPosition() * COUNTS_PER_INCH;
-        LBDistanceInches = motorLeftBack.getCurrentPosition() * COUNTS_PER_INCH;
-        RDistanceInches = motorRight.getCurrentPosition() * COUNTS_PER_INCH;
-        RBDistanceInches = motorRightBack.getCurrentPosition() * COUNTS_PER_INCH;
-
-        /*rightBackDifference = RBDistanceInches - previousPosRB;
-        rightDifference = RDistanceInches - previousPosR;
-        leftBackDifference = LBDistanceInches - previousPosLB;
-        leftDifference = LDistanceInches - previousPosL;*/
 
 
         double h = Math.hypot(gamepad1.left_stick_x, -gamepad1.left_stick_y);
@@ -77,27 +41,10 @@ public class    mecanum extends OpMode {
         final double v4 = h * Math.cos(robotAngle) - rightX;
 
 
-
-        /*if(gamepad1.left_stick_x == 0.0 && gamepad1.left_stick_y == 0.0 && gamepad1.right_stick_x == 0.0 && leftDifference > 0.1) {
-
-
-
-        }*/
-
         motorLeft.setPower(-v1 * 4/5);
         motorRight.setPower(v2 * 4/5);
         motorLeftBack.setPower(-v3 * 4/5);
         motorRightBack.setPower(v4 * 4/5);
 
-        /*if (runtime.milliseconds() >= 100) {
-
-            previousPosL = LDistanceInches;
-            previousPosLB = LBDistanceInches;
-            previousPosR = RDistanceInches;
-            previousPosRB = RBDistanceInches;
-
-            runtime.reset();
-
-        }*/
     }
 }
