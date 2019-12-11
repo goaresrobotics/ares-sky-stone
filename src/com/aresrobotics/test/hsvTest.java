97191a8981064548a74c95daeb9ff440ad3d1b35
charlotteec <blipbleepbloop@gmail.com>
2019-12-10 17:37:26 -0800

    color test

--- src/com/aresrobotics/test/hsvTest.java
+++ src/com/aresrobotics/test/hsvTest.java
@@ -6,19 +6,9 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 @TeleOp(name = "hsvColor")
 public class hsvTest extends OpMode {
+
     ColorSensor senseColor;
     double red;
-    double green;
-    double blue;
-    double redPrevious;
-    double greenPrevious;
-    double bluePrevious;
-    double max;
-    double min;
-    double change;
-    double value;
-    double saturation;
-    double hue;
 
     public void init() {
 
@@ -26,83 +16,24 @@ public class hsvTest extends OpMode {
 
     }
 
-    public void loop() {
 
+    public void loop() {
+        
         senseColor.enableLed(false);
 
-        redPrevious = senseColor.red();
-        greenPrevious = senseColor.green();
-        bluePrevious = senseColor.blue();
-
-        red = redPrevious / 255;
-        green = greenPrevious / 255;
-        blue = bluePrevious / 255;
-
-        if (red > green) {
-            if (red > blue) {
-                max = red;
-            } else {
-                max = blue;
-            }
-        } else {
-            if (green > blue) {
-                max = green;
-            } else {
-                max = blue;
-            }
-        }
-
-        if (red < blue) {
-            if (red < green) {
-                min = red;
-            } else {
-                min = green;
-            }
-        } else {
-            if (blue < green) {
-                min = blue;
-            } else {
-                min = green;
-            }
-        }
-
-
-        change = max - min;
-
-        value = max;
-
-        if (change == 0) {
-            hue = 0;
-        } else {
-            if (max == red) {
-                hue = (green - blue) / change;
-            } else {
-                if (max == green) {
-                    hue = ((blue - red) / change) + 2;
-                } else {
-                    if (max == blue) {
-                        hue = ((red - green) / change) + 4;
-                    }
-                }
-            }
-        }
-
-        if (value == 0) {
-            saturation = 0;
-        } else {
-            saturation = change / value;
-        }
+        red = senseColor.red();
 
         telemetry.addData("Red", red);
-        telemetry.addData("Green", green);
-        telemetry.addData("Blue", blue);
-        telemetry.addData("Hue", hue);
-        telemetry.addData("Saturation", saturation);
-        telemetry.addData("Value", value);
-        telemetry.addData("Min", min);
-        telemetry.addData("Max", max);
-        telemetry.addData("Change", change);
         telemetry.update();
+
+
+
+        while(red<70) {
+            red = senseColor.red();
+            telemetry.addLine("Got Skystone");
+            telemetry.addData("Red", red);
+            telemetry.update();
+        }
 
     }
 
