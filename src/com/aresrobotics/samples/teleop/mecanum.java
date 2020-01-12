b590a6a1e73e999e89e39dbb3a87a53cfcedb009
Jason <jjmack921@gmail.com>
2020-01-12 15:54:33 -0800

    finished take one of field centric drive

--- src/com/aresrobotics/samples/teleop/mecanum.java
+++ src/com/aresrobotics/samples/teleop/mecanum.java
@@ -34,6 +34,9 @@ public class    mecanum extends OpMode {
 
     }
 
+    double newX = 0;
+    double newY = 0;
+
     @Override
     public void loop()
     {
@@ -44,16 +47,31 @@ public class    mecanum extends OpMode {
         double x = gamepad1.left_stick_x;
         double y = gamepad1.left_stick_y;
         double power = Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
-        double angleToZero = Math.acos(y/power);
+        double joystickAngle = Math.atan(x/y);
+        double joystickRobotAngle = angle-joystickAngle;
+        double newRobotAngle = joystickAngle-joystickRobotAngle;
 
-        if(angleToZero-2*Math.PI*power>0)
+
+        if((newRobotAngle<=90 && newRobotAngle>0)||(newRobotAngle>360 && newRobotAngle<=450)||(newRobotAngle<=-270&&newRobotAngle>-360))
         {
-
-
-
+            newX = -Math.abs(power*(Math.cos(angle-Math.acos(x/power))));
+            newY = Math.abs(power*(Math.sin(angle-Math.acos(x/power))));
         }
-        double newX = power*(Math.cos(angle-Math.acos(x/power)));
-        double newY = power*(Math.sin(angle-Math.acos(x/power)));
+        if((newRobotAngle<=180 && newRobotAngle>90)||(newRobotAngle>450 && newRobotAngle<=540)||(newRobotAngle<=-180&&newRobotAngle>-270))
+        {
+            newX = -Math.abs(power*(Math.cos(angle-Math.acos(x/power))));
+            newY = -Math.abs(power*(Math.sin(angle-Math.acos(x/power))));
+        }
+        if((newRobotAngle<=-90 && newRobotAngle>-180)||(newRobotAngle>180 && newRobotAngle<=270)||(newRobotAngle<=-450&&newRobotAngle>-540))
+        {
+            newX = Math.abs(power*(Math.cos(angle-Math.acos(x/power))));
+            newY = -Math.abs(power*(Math.sin(angle-Math.acos(x/power))));
+        }
+        if((newRobotAngle<=0 && newRobotAngle>-90)||(newRobotAngle>270 && newRobotAngle<=360)||(newRobotAngle<=-360&&newRobotAngle>-450))
+        {
+            newX = Math.abs(power*(Math.cos(angle-Math.acos(x/power))));
+            newY = Math.abs(power*(Math.sin(angle-Math.acos(x/power))));
+        }
 
         double h = Math.hypot(newX, -newY);
         double robotAngle = Math.atan2(-newY, newX) - Math.PI / 4;
