c8edfc297633488d42d82755d4af5f0a4fc49ed7
Jason <jjmack921@gmail.com>
2020-02-08 13:05:03 -0800

    Change method for turning to have a mode with a higher PCoefficient. Tested and fixed more paths. Added stone grab and foundation grab path.

--- src/com/aresrobotics/auto/grabBlockGrabTrayBlue.java
+++ src/com/aresrobotics/auto/grabBlockGrabTrayBlue.java
@@ -9,27 +9,31 @@ public class grabBlockGrabTrayBlue extends Auto{
     @Override
     public void run() {
 
-        timer();
         encoderDrive(0.4, 0.4, 46.5, 46.5, 3);
-        encoderDrive(0.4, 0.4, -4.6, -4.6, 2);
+        encoderDrive(0.4, 0.4, -6.5, -6.5, 2);
         turn(-90, 2, false);
         intake(true, true);
         encoderDrive(0.4, 0.4, 8, 8, 2);
         encoderDrive(0.4, 0.4, -8, -8, 2);
         intake(false, false);
-        strafe(true, 950);
-        turn(88, 2, false);
+        turn(90, 3, true);
+        strafe(false, 1000);
+        turn(90, 2, true);
         encoderDrive(0.8, 0.8, 72, 72, 5);
-        turn(175, 2, false);
+        turn(177.5, 2, false);
         encoderDrive(0.6, 0.6, -10, -10, 2);
         trayGrab();
         sleep(200);
-        encoderDrive(0.15, 0.525, 57.5,57.5, 8);
-        turn(-90, 1, false);
+        encoderDrive(0.15, 0.575, 57.5,57.5, 8);
+        turn(-90, 1, true);
         encoderDrive(0.4, 0.4, -15, -15, 3);
-        turn(-90,1, false);
+        turn(-90,1, true);
         trayRelease();
-        encoderDrive(0.8, 0.8,35, 35, 6);
+        encoderDrive(0.4, 0.4, 10, 10, 5);
+        turn(90, 3, true);
+        intake(true, false);
+        encoderDrive(0.4, 0.4,-25, -25, 6);
+        intake(false,false);
 
     }
 }
