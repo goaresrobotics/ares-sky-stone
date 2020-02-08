78a9af046e5e1c093e105d24eb17ccbf4544e42f
charlotteec <blipbleepbloop@gmail.com>
2020-02-08 12:47:42 -0800

    create an auto lift retraction mechanism have not tested yet

--- src/com/aresrobotics/finalCodeSkystone/finalTeleop.java
+++ src/com/aresrobotics/finalCodeSkystone/finalTeleop.java
@@ -1,6 +1,7 @@
 package com.aresrobotics.finalCodeSkystone;
 
 import com.aresrobotics.subSystems.arm;
+import com.aresrobotics.subSystems.autoRetract;
 import com.aresrobotics.subSystems.intake;
 import com.aresrobotics.subSystems.lift;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -19,6 +20,7 @@ public class finalTeleop extends OpMode{
     private com.aresrobotics.subSystems.lift lift = new lift();
     private trayGrabber grabber = new trayGrabber();
     private driveBase drive = new driveBase();
+    private com.aresrobotics.subSystems.autoRetract autoRetract = new autoRetract();
 
     @Override
     public void init() {
@@ -37,6 +39,7 @@ public class finalTeleop extends OpMode{
         lift.runLift(gamepad2.dpad_down, gamepad2.dpad_up, gamepad2.left_trigger, gamepad2.right_trigger, gamepad2.x, gamepad2.y, gamepad2.left_bumper, gamepad2.right_bumper, gamepad2.left_stick_y);
         grabber.runGrabber(gamepad1.right_bumper, gamepad1.left_bumper);
         drive.runDrive(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x, gamepad1.b, gamepad1.a);
+        autoRetract.retractLift(gamepad2.dpad_down);
 
     }
 }
